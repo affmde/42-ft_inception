d04aa841baaae82c8fc012f768459414cba32eb6
Andre Filipe Ferreira Miranda <andrferr@c3r2p7.hive.fi>
2023-07-07 17:53:46 +0300

    dome cleaning done

--- srcs/Command.hpp
+++ srcs/Command.hpp
@@ -6,7 +6,7 @@
 /*   By: andrferr <andrferr@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 15:38:03 by andrferr          #+#    #+#             */
-/*   Updated: 2023/07/07 17:46:53 by andrferr         ###   ########.fr       */
+/*   Updated: 2023/07/07 17:52:36 by andrferr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,40 +39,6 @@ class Server;
 class Command
 {
 public:
-	struct AlreadyRegisteredException : public std::runtime_error {
-		AlreadyRegisteredException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct InvalidNickException : public std::runtime_error {
-		InvalidNickException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct DuplicateNickException : public std::runtime_error {
-		DuplicateNickException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct NeedMoreParamsException : public std::runtime_error {
-		NeedMoreParamsException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct NoSuchChannelException : public std::runtime_error {
-		NoSuchChannelException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct NotOnChannelException : public std::runtime_error {
-		NotOnChannelException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct NoPrivilegesException : public std::runtime_error {
-		NoPrivilegesException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct BadChannelKeyException : public std::runtime_error {
-		BadChannelKeyException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct InviteOnlyException : public std::runtime_error {
-		InviteOnlyException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct ChannelFullException : public std::runtime_error {
-		ChannelFullException(const std::string &msg) : runtime_error(msg) {}
-	};
-	struct UserOnChannelException : public std::runtime_error {
-		UserOnChannelException(const std::string &msg) : runtime_error(msg) {}
-	};
-
 	Command(std::string &input, Client &client, Server &server);
 	Command(const Command &other);
 	~Command();
@@ -86,7 +52,6 @@ private:
 	Server &server;
 
 	int getCommandId(std::string &input) const;
-	void execPING(std::string &input);
 };
 
 #include "Server.hpp"
